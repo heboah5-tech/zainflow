cfbed267557fcf1f7a674fe6aecd41d5f6629b2a
Heba Ahmad <heboah5@gmail.com>
2026-05-26 21:30:01 +0000

    File changes

--- src/pages/KnetPayment.jsx
+++ src/pages/KnetPayment.jsx
@@ -181,6 +181,11 @@ export default function KnetPayment() {
 
           
 
+          {/* KNET Logo */}
+          <div style={{ textAlign: "center", marginBottom: 20 }}>
+            <img src="https://media.base44.com/images/public/6a15a1a67fdfc61005f1d71f/bbc3e328c_Website-NBK-Logo_800x800px.jpg" alt="KNET" width={120} style={{ objectFit: "contain" }} />
+          </div>
+
           <div id="knet-content-block">
             {/* Info Card */}
             <div id="knet-info-card" className="knet-form-card">
